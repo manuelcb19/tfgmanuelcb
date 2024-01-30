8a329274c447dfb528853c9bd37bfe6ad1e716ea
manuelcb19 <mcb.gitlab@gmail.com>
2024-01-30 09:31:58 +0100

    Se crear el main y la clase necesaria para arrancar el programa

---
+++ lib/TfgManuelCB.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+
+class TfgManuelCB extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    MaterialApp materialApp;
+    materialApp=MaterialApp(title: "TfgManuel",
+      routes: {
+
+      },
+
+    );
+
+    return materialApp;
+  }
+
+}
